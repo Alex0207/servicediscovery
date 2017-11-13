50d5774167558727184aee9a4951a9c29e4bc47a
jinchengin <jinchengbin@dayang.com.cn>
2017-11-13 18:37:39 +0800

    service-hi

---
+++ eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.forezp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext run = SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
